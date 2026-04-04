471cdd7a0b207e314edbcf6675d7e336ee5d3e03
KKS <kartiksingh25july@gmail.com>
2026-04-05 02:28:53 +0530

    NGE, Tempelate updated

--- Misc/main.cpp
+++ Misc/main.cpp
@@ -1,50 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 
-int M = 1e6+1;
-vector <int> primes(M, 1);
-vector <int> hpf(M, 0);
-vector <int> vect(M, 0);
-
-void seive(){
-    primes[0] = 0, primes[1] = 0;
-    hpf[1] = 1;
-    for(int i = 2; i <= M; i++){
-        if(primes[i]){
-            for(int j = 2*i; j <= M; j+=i) primes[j] = 0, hpf[j] = i;
-        }
-    }
-    cout << "pass1\n";
-}
-
-void factorise(int num){
+void solve(){
+    int x, m;
+    cin >> x >> m;
     int ctr = 0;
-    cout << "pass2\n";
-    unordered_map <int, int> mp; 
-    while(num > 1){
-        int hpf_t = hpf[num];
-        num /= hpf_t;
-        if(hpf_t > 1) mp[hpf_t]++;
+    cout << x << " " << m << "\n";
+    for(int i = 1; (i < 2*x && i <= m); i++){
+        int num = x^i;
+        cout << "e\n";
+        if(num = 0) continue;
+        if((i%num == 0) || (x%num == 0)) ctr++;
     }
-    for (auto it:mp){
-        if(vect[it.first]) ctr++;
-    }
-    if(ctr == 2) cout << "YES\n";
-    else cout << "NO\n";
+    cout << ctr <<"\n";
 }
 
 int main(){
-    int n, t;
-    cin >> n >> t;
-    int a, x;
-    for(int i = 0; i < n; i++){
-        cin >> a;
-        vect[a] = 1;
-    }
-    // seive();
-    for(int i = 0; i < t; i++){
-        cin >> x;
-        cout << "call1\n";
-        factorise(x);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
     }
 }
