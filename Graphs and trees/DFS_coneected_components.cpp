a656600020b4b0a514a0e77cb129613098a83765
KKS <kartiksingh25july@gmail.com>
2026-04-18 16:24:55 +0530

    revised graphs till cycle detection via luv

--- Graphs and trees/DFS_coneected_components.cpp
+++ Graphs and trees/DFS_coneected_components.cpp
@@ -4,11 +4,17 @@
 
 using namespace std;
 
-const int N = 10e5; // for max possible size of 2-d array N*N
-bool visited_array[N];
-vector <vector<int>> list_of_all_connected_components;
-vector<int> temp_comoponet_list;
+//logic:- 1. if we run dfs from a node then all of it's connected compoents will become visited
+//        2. so simply go through all nodes and if that node is unvisited then simply call dfs in that node and number of dfs calls is the
+//           number of connected components.
 
+const int N = 10e3+5;
+bool visited_array[N];
+vector <vector<int>> list_of_all_connected_components;  // to store all CC
+vector<int> temp_comoponet_list;    // to temporarly store a connected component
+
+
+//feel that orignal DFS struct is untouched.
 void dfs(array <vector<int>, N> graph2, int vertex){
 
     visited_array[vertex] = 1;
@@ -16,10 +22,7 @@ void dfs(array <vector<int>, N> graph2, int vertex){
 
     for(int child : graph2[vertex]){
 
-        if(visited_array[child] == 1){
-            continue;
-        }
-
+        if(visited_array[child]) continue;
         dfs(graph2, child);
     }
 }
@@ -34,28 +37,23 @@ int main(){
     array <vector<int>, N> graph2;
     for(int i = 0; i < number_of_edges; i++){
         cin >> x >> y;
-
         graph2[y].push_back(x);
         graph2[x].push_back(y);
     }
 
-    //now counting and storing connected components:
-    //note ---> 1 dfs starting from any node in a connected component will visit every other node in that connected component, so count = number of dfs required.
     int count = 0;
 
     for(int i = 1; i <= number_of_nodes; i++){
-        //this if() ensures that all connected components are visited exactly once
-        if(visited_array[i] == true){
-            continue;
-        }
-        else{
-            temp_comoponet_list.clear();
-            dfs(graph2, i);
-            list_of_all_connected_components.push_back(temp_comoponet_list);
-            count ++;
-        }
+        
+        if(visited_array[i] == true) continue;    //this if() ensures that all connected components are visited exactly once
+
+        temp_comoponet_list.clear();
+        dfs(graph2, i);
+        list_of_all_connected_components.push_back(temp_comoponet_list);
+        count ++;
     }
 
+    cout << count << "\n";
     for(int i = 0; i < count; i++){
         for(auto j: list_of_all_connected_components[i]){
             cout << j <<" ";
--- Graphs and trees/DFS_cycle_detection.cpp
+++ Graphs and trees/DFS_cycle_detection.cpp
@@ -4,23 +4,26 @@
 
 using namespace std;
 
+
 const int N = 10e3;
 array <vector<int>, N> graph;
 bool visited_lst[N];
-bool isCycleExist = false;
 
-bool dfs(int vertex, int parent){               // parent node of current vertex to keep in mind so that we can ignore it checking for condition of cycle.
-
+//logic:- 
+// 1. if we encounter an already visited node while traversal and it is not the node that we came from then cycle exist.
+// 2. so maintain a var "parent" node of current vertex, so that we can ignore checking it for condition of cycle
+bool dfs(int vertex, int parent){  
     visited_lst[vertex] = 1;
-    for(int child : graph[vertex]){
-        if(child == parent){
-            continue;
-        }
-        if(visited_lst[child]){
-            return true;                //as soon as even a single cycle is found return true and then fall back to parent node(we could add something to even exit dfs right now to optimise)
-        }
+    bool isCycleExist = false;              //feel that for every vertex we are checking if a cycle exist with current vertex as starting
+                                            //point, hence initialising it with false initially.
+    for(int child : graph[vertex]){         
+        if(child == parent) continue;
+        if(visited_lst[child]) return true; //note that as soon as a visited_non_current_parent node is found then cycle detected in the
+                                            //current vertex.
         isCycleExist |= dfs(child, vertex);
     }
+    return isCycleExist;                    //as traversal for all the childs finishes return if cycle exist for current vertex(taken-or
+                                            //with all vertices traversed yet).
 }
 
 int main(){
@@ -35,14 +38,8 @@ int main(){
     }
 
     for(int i = 1; i <= n; i++){
-        if(visited_lst[i] == 1){
-            continue;
-        }
-        dfs(i,1);
-        if(isCycleExist == 1){
-            cout << "cycle exist\n";
-            return 1;
-        }
+        if(visited_lst[i] == 1) continue;
+        if(dfs(i,1)) {cout << "cycle exist\n"; return 0;}
     }
-    return 0;
+    cout << "no cycle\n";
 }
